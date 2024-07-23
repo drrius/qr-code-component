7fae13cf417856ce5acff8319b25f5f94a69a0c9
Darius Sibarium <dsibarium@gmail.com>
2024-07-23 11:33:39 +0200

    remove random semi colon at bottom of screen

--- qr-code-component/app/page.tsx
+++ qr-code-component/app/page.tsx
@@ -4,7 +4,7 @@ import MainPage from "./components/MainPage";
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <MainPage />;
+      <MainPage />
     </div>
   );
 }
